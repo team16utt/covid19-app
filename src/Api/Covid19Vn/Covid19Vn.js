6bfdb7a8af21838ae2b3db4043359fe27091da44
Tuấn Dương <tuanduong2920@gmail.com>
2021-06-07 14:17:24 +0700

    update

--- src/Api/Covid19Vn/Covid19Vn.js
+++ src/Api/Covid19Vn/Covid19Vn.js
@@ -2,13 +2,13 @@ import axiosClient from "../AxiosClient";
 
 const Covid19Vn = {
   getProvinces: async () => {
-    const res = axiosClient.get("https://thingproxy.freeboard.io/fetch/http://45.32.102.61:8080/api/provinces", {
+    const res = axiosClient.get("http://45.32.102.61:8080/api/provinces", {
       baseURL: "",
     });
     return res;
   },
   getPatients: async () => {
-    const res = axiosClient.get("https://thingproxy.freeboard.io/fetch/http://45.32.102.61:8080/api/vn_patients", {
+    const res = axiosClient.get("http://45.32.102.61:8080/api/vn_patients", {
       baseURL: "",
     });
     return res;
